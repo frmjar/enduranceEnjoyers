c53092b051eafddc249f4f4e310c0115290f0751
Félix Redondo <frmanzanares@hotmail.com>
2025-07-17 13:19:16 +0200

    Refactor project structure and remove unused components
    - Removed dependencies for @astrojs/node and @astrojs/react from package.json.
    - Deleted Calculadora component and its associated API endpoint.
    - Updated Contact component to simplify form handling and changed action URL.
    - Refactored image handling by moving Cloudinary functions to a new libs/cloudinary.ts file.
    - Updated Galeria and TeamDescription components to use the new Cloudinary functions.
    - Deleted contacto.astro and calculadora.astro pages as they are no longer needed.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
-import node from '@astrojs/node'
-import react from '@astrojs/react'
 import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  integrations: [react()],
-
-  adapter: node({
-    mode: 'standalone'
-  }),
-
   vite: {
     plugins: [tailwindcss()]
   },
